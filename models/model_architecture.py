4f9754a56a88500c3ae647399b5f02ea9006fbe2
renato boemer <boemer00@gmail.com>
2023-12-14 10:43:50 +0000

    model architecture and tests

--- models/model_architecture.py
+++ models/model_architecture.py
@@ -5,49 +5,35 @@ from keras.optimizers import Adam
 from keras.regularizers import l2
 from keras.metrics import Recall
 
-def build_model(input_shape=(224, 224, 3), trainable=False):
-    """
-    builds a image classification model based on a pre-trained VGG19 architecture.
+class SignetRingModel:
+    def __init__(self, input_shape=(224, 224, 3), trainable=False):
+        self.input_shape = input_shape
+        self.trainable = trainable
+        self.model = self.build_model()
 
-    :param input_shape: tuple, shape of the input images
-    :param trainable: bool, whether the VGG19 layers should be trainable
-    :return: Compiled Keras model.
-    """
-    # load pre-trained VGG19 model
-    base_model = VGG19(weights='imagenet', include_top=False, input_shape=input_shape)
+    def build_model(self):
+        base_model = VGG19(weights='imagenet', include_top=False, input_shape=self.input_shape)
+        for layer in base_model.layers:
+            layer.trainable = self.trainable
 
-    # freeze base model layers
-    for layer in base_model.layers:
-        layer.trainable = trainable
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(512, activation='relu', kernel_regularizer=l2(0.01))(x)
+        x = Dropout(0.5)(x)
+        predictions = Dense(1, activation='sigmoid')(x)
+        return Model(inputs=base_model.input, outputs=predictions)
 
-    # add custom layers
-    x = base_model.output
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(512, activation='relu', kernel_regularizer=l2(0.01))(x)
-    x = Dropout(0.5)(x)
-    predictions = Dense(1, activation='sigmoid')(x)  # binary 1 or 0
-
-    # define the complete model
-    model = Model(inputs=base_model.input, outputs=predictions)
-
-    return model
-
-def compile_model(model, learning_rate=0.001):
-    """
-    compiles the model
-    """
-    model.compile(optimizer=Adam(learning_rate=learning_rate),
-                  loss='binary_crossentropy',
-                  metrics=['accuracy', Recall()])
-    return model
+    def compile(self, learning_rate=0.001):
+        self.model.compile(optimizer=Adam(learning_rate=learning_rate),
+                           loss='binary_crossentropy',
+                           metrics=['accuracy', Recall()])
 
 def main():
-    # build and compile the model
-    model = build_model()
-    compiled_model = compile_model(model)
-    model.summary()
+    signet_model = SignetRingModel()
+    signet_model.compile()
+    signet_model.model.summary()
 
-    return compiled_model
+    return signet_model
 
 if __name__ == "__main__":
-    model = main()
+    model_instance = main()
--- tests/test_model_script.py
+++ tests/test_model_script.py
@@ -1,22 +1,22 @@
 import pytest
-from models.model_architecture import build_model, compile_model
+from models.model_architecture import SignetRingModel
 
 def test_build_model():
     """
     test if the model's architecture is built correctly with the correct input shape
     """
     input_shape = (224, 224, 3)
-    model = build_model(input_shape)
+    signet_model = SignetRingModel(input_shape=input_shape)
 
     # check if the input shape of the model matches the required shape
-    assert model.input_shape == (None, *input_shape), "model input shape is incorrect"
+    assert signet_model.model.input_shape == (None, *input_shape), "model input shape is incorrect"
 
 def test_compile_model():
     """
-    Test if the model compiles without any errors.
+    test if the model compiles without any errors.
     """
-    model = build_model()
-    compiled_model = compile_model(model)
+    signet_model = SignetRingModel()
+    signet_model.compile()
 
     # models should have an optimiser to compile correctly
-    assert compiled_model.optimizer is not None, "model did not compile correctly"
+    assert signet_model.model.optimizer is not None, "model did not compile correctly"
